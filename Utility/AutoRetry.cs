e6cabf8fa2343e8f542860c98760fcf504887ce1
ORelio <oreliogitantispam.l0gin@spamgourmet.com>
2014-11-29 23:58:47 +0100

    Add AutoRetry class

---
+++ Utility/AutoRetry.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace SharpTools
+{
+    /// <summary>
+    /// Allow easy retries on pieces of code
+    /// By ORelio - (c) 2014 - CDDL 1.0
+    /// </summary>
+    public class AutoRetry
+    {
+        /// <summary>
+        /// Perform the specified action, retrying if an exception is caught.
+        /// If last attempt fails, exception is not caught.
+        /// </summary>
+        /// <param name="action">Action to run</param>
+        /// <param name="attempts">Maximum amount of attempts</param>
+        public static void Perform(Action action, int attempts = 3)
+        {
+            Perform(action, null, attempts);
+        }
+
+        /// <summary>
+        /// Perform the specified action, retrying after performing the specified onError action if an exception is caught.
+        /// If last attempt fails, exception is not caught and onError is not performed.
+        /// </summary>
+        /// <param name="action">Action to run</param>
+        /// <param name="onError">Action to run before retrying if an error occured</param>
+        /// <param name="attempts">Maximum amount of attempts</param>
+        public static void Perform(Action action, Action onError, int attempts = 3)
+        {
+            if (attempts < 1)
+                attempts = 1;
+
+            bool success;
+
+            do
+            {
+                success = true;
+                try { action(); }
+                catch
+                {
+                    success = false;
+                    if (attempts <= 1)
+                    {
+                        throw;
+                    }
+                    else if (onError != null)
+                    {
+                        onError();
+                    }
+                }
+                attempts--;
+            } while (!success && attempts > 0);
+        }
+    }
+}
