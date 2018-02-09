554cb0db468a0c422b9ebfd8b2e6a067c8eb2e02
ORelio <oreliogitantispam.l0gin@spamgourmet.com>
2018-02-09 21:56:03 +0100

    Add FileSystemAdmin and Lockscreen
    FileSystemAdmin: TakeOwn+FullControl => Administrators group
    Lockscree: Wrapper around Win10 lockscreen (for Spotlight Downloader)

---
+++ WinAPI/Lockscreen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using SharpTools;
+
+namespace SharpTools
+{
+    /// <summary>
+    /// Wrapper around the Windows 10 lockscreen
+    /// By ORelio (c) 2018 - CDDL 1.0
+    /// </summary>
+    /// <remarks>
+    /// Requires ../Utility/FileSystemAdmin.cs and ../Utility/Privilege20 in order to compile.
+    /// </remarks>
+    class Lockscreen
+    {
+        /// <summary>
+        /// Replace the Windows 10 global lockscreen
+        /// </summary>
+        /// <remarks>Administrator permissions are required</remarks>
+        /// <param name="path">Path to the new .jpg file for global lockscreen</param>
+        public static void SetGlobalLockscreen(string path)
+        {
+            string systemDataDir = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Microsoft\Windows\SystemData";
+            string systemDataCache = systemDataDir + @"\S-1-5-18\ReadOnly\LockScreen_Z";
+            string lockscreenDir = Environment.GetFolderPath(Environment.SpecialFolder.Windows) + @"\Web\Screen";
+            string lockscreenPic = lockscreenDir + @"\img100.jpg";
+            string lockscreenBak = lockscreenDir + @"\img200.jpg";
+
+            FileSystemAdmin.GrantAll(systemDataDir, true);
+            FileSystemAdmin.GrantAll(lockscreenDir, true);
+
+            if (!File.Exists(lockscreenBak))
+                File.Copy(lockscreenPic, lockscreenBak);
+            File.Copy(path, lockscreenPic, true);
+
+            FileSystemAdmin.DeleteContents(systemDataCache);
+        }
+    }
+}
